044e25ee42a77b0124f2288316795a9a38454a00
mdshahriurahmed <shahriurahmedcse@gmail.com>
2026-07-27 03:43:54 +0600

    banner added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <div className='relative'>
+                <Banner></Banner>
+                <div className='absolute left-32 bottom-36'>
+                    <button className="btn bg-primary rounded-full text-secondary px-7">Track Your Parcel</button>
+                </div>
+            </div>
         </div>
     );
 };
